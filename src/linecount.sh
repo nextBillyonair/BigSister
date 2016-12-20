13f2cade2b8875f28f15b3aad74dbbdf314ea4fb
billyonair <billyonair@acm.jhu.edu>
2016-12-20 02:43:51 -0500

    delete old file

--- src/linecount.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-printf "TOTAL LINE COUNT:\n"
-git ls-files | xargs wc -l
-printf "\nCode Files Word Count:\n"
-git ls-files | grep '.py\|.sh\|.html' | grep -v '.pyc' | xargs wc -l
